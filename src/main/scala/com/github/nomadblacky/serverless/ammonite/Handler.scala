2b3f8d37b06e78931a6ec1be4259b905c66e9944
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-06-01 07:59:22 +0900

    WIP: Implement to execute ammonite scripts

--- src/main/scala/com/github/nomadblacky/serverless/ammonite/Handler.scala
+++ src/main/scala/com/github/nomadblacky/serverless/ammonite/Handler.scala
@@ -1,10 +1,30 @@
 package com.github.nomadblacky.serverless.ammonite
 
+import ammonite.util.Res._
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
 class Handler extends RequestHandler[Request, Response] {
 
   def handleRequest(input: Request, context: Context): Response = {
-    Response("Go Serverless v1.0! Your function executed successfully!", input)
+    val (res, _) = ammonite
+      .Main(
+        // Configuration REPL
+      )
+      .runCode(
+        """println("Hello Ammonite!")"""
+      )
+
+    val message = res match {
+      case Success(_) => "OK"
+      case f: Failing =>
+        f match {
+          case Failure(msg)      => msg
+          case Exception(t, msg) => s"""$msg\n${t.getStackTrace.mkString("\n")}"""
+          case Skip              => "Skipped"
+          case Exit(value)       => s"Exit: $value"
+        }
+    }
+
+    Response(res.isSuccess, message, input)
   }
 }
--- src/main/scala/com/github/nomadblacky/serverless/ammonite/Response.scala
+++ src/main/scala/com/github/nomadblacky/serverless/ammonite/Response.scala
@@ -2,4 +2,8 @@ package com.github.nomadblacky.serverless.ammonite
 
 import scala.beans.BeanProperty
 
-case class Response(@BeanProperty message: String, @BeanProperty request: Request)
+case class Response(
+    @BeanProperty isSuccess: Boolean,
+    @BeanProperty message: String,
+    @BeanProperty request: Request
+)
